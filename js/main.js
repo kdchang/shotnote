f7f23615d2bd74249e820c88cc1091322ef65510
KD-Chang <ibelieveicaner@gmail.com>
2013-11-24 17:01:12 +0800

    add backbone

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+jQuery(function($){
 
+	window.AppView = Backbone.View.extend({
+		el: $('#wrap'),
+
+		event: {
+			'click #camera': 'cameraActivity'
+		},
+
+		initialize: function(){
+			console.log('AppView');
+		},
+
+		cameraActivity: function(){
+			var getphoto = new MozActivity({
+ name: "pick",
+ data: {
+ type: ["image/png", "image/jpg", "image/jpeg"]
+ }
+ });
+
+ getphoto.onsuccess = function () {
+ var img = document.createElement("img");
+ if (this.result.blob.type.indexOf("image") != -1) {
+ img.src = window.URL.createObjectURL(this.result.blob);
+ }
+ };
+
+ getphoto.onerror = function () {
+ // erro!
+ };
+		}
+	});
+	window.App = new AppView;
+})
